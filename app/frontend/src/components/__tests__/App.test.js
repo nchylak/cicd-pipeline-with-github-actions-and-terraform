080b1af00c89572ea547fd3c80e2b2e3ee52f6c7
nchylak <nadia@macnad.local>
2025-08-29 21:16:30 +0200

    put back import

--- app/frontend/src/components/__tests__/App.test.js
+++ app/frontend/src/components/__tests__/App.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
+
 import App from '../../App';
 
 const movieHeading = process.env.FAIL_TEST ? 'WRONG_HEADING' : 'Movie List';
